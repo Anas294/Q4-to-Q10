f0981b42d4daa8a4bca36b935d0d91c27386b108
( Muhammad Anas) <(anas923002948053@gmail.com)>
2024-04-03 01:46:25 +0500

    third comment

---
+++ main.js
@@ -0,0 +1,41 @@
+//assiment no 04
+var famqoute = "A person who never made a mistake never tried anything new";
+var auther = "Albert Einstein";
+console.log("".concat(auther, " once said,\"").concat(famqoute, "\""));
+//assiment no 05
+var famousperson = "Albert Einstein";
+var message = "".concat(famousperson, ", once said, \"").concat(famqoute, "\"");
+console.log("message");
+//assiment no 06
+var nameWithWiteSpace = "\t \n Muhammad Anas \t \n";
+console.log("name with wite space", nameWithWiteSpace);
+var strippname = nameWithWiteSpace.trim();
+console.log("stripp Name:", strippname);
+//assiment no 07
+var additionResult = 5 + 3;
+console.log("addition result", additionResult);
+var subtractionResult = 10 - 2;
+console.log("subtraction result", subtractionResult);
+var multiplicationResult = 4 * 2;
+console.log("multiplication result", multiplicationResult);
+var divisionResult = 16 / 2;
+console.log("division result", divisionResult);
+//assiment no 08
+console.log(5 + 3);
+console.log(10 - 2);
+console.log(4 * 2);
+console.log(16 / 2);
+//assiment no 09
+var fevorateResult = 8;
+var messageResult = 'My Favorate number: (favoratResult)';
+console.log(messageResult);
+// Ctrl + question
+//assiment no 10
+// this program adds two number and print the result
+// define two numbers
+var number1 = 10;
+var number2 = 20;
+// add two numbers
+var result = number1 + number2;
+// print result
+console.log(result);
---
+++ main.ts
@@ -0,0 +1,62 @@
+//assiment no 04
+let famqoute:string = "A person who never made a mistake never tried anything new";
+let auther:string = "Albert Einstein";
+
+console.log(`${auther} once said,"${famqoute}"`);
+
+//assiment no 05
+let famousperson:string = "Albert Einstein";
+let message: string =`${famousperson}, once said, "${famqoute}"`;
+
+console.log("message");
+
+//assiment no 06
+let nameWithWiteSpace:string = "\t \n Muhammad Anas \t \n";
+console.log("name with wite space", nameWithWiteSpace);
+
+let strippname:string = nameWithWiteSpace.trim();
+console.log("stripp Name:", strippname);
+
+//assiment no 07
+let additionResult : number = 5+3;
+
+console.log("addition result", additionResult);
+
+let subtractionResult : number = 10-2;
+
+console.log("subtraction result", subtractionResult);
+
+let multiplicationResult : number = 4*2;
+
+console.log("multiplication result", multiplicationResult);
+
+let divisionResult : number = 16/2;
+
+console.log("division result", divisionResult);
+
+//assiment no 08
+console.log(5+3);
+console.log(10-2);
+console.log(4*2);
+console.log(16/2);
+
+//assiment no 09
+let fevorateResult: number = 8;
+
+let messageResult:string ='My Favorate number: (favoratResult)';
+
+console.log(messageResult,);
+
+// Ctrl + question
+
+//assiment no 10
+
+// this program adds two number and print the result
+
+// define two numbers
+let number1: number = 10;
+let number2: number = 20;
+// add two numbers
+let result:number = number1 + number2;
+// print result
+console.log(result);
